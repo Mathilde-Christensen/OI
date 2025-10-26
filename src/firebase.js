53f55066c6df35291d1334c6d28aeca0e0c32d2c
k1rja <kirjabanke1999@gmail.com>
2025-10-26 17:13:07 +0100

    api tilføjet i env

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,17 @@
 import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
+// (valgfrit til senere login hvis I vil): 
+// import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAI3EVPghGVbjhd8SP2ySFV0ZoeIQJw2rE",
-    authDomain: "frontend-projekt-98306.firebaseapp.com",
-    databaseURL: "https://frontend-projekt-98306-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "frontend-projekt-98306",
-    storageBucket: "frontend-projekt-98306.firebasestorage.app",
-    messagingSenderId: "1046447085483",
-    appId: "1:1046447085483:web:84f403e9a7a2de1c2a24ee"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'frontend-projekt-98306.firebaseapp.com',
+  databaseURL: 'https://frontend-projekt-98306-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'frontend-projekt-98306',
+  storageBucket: 'frontend-projekt-98306.appspot.com',
+  messagingSenderId: '1046447085483',
+  appId: '1:1046447085483:web:84f403e9a7a2de1c2a24ee',
+}
 
-  // Opret forbindelse til Realtime Database
-  export const db = getDatabase(app);
+export const app = initializeApp(firebaseConfig)
+export const db = getDatabase(app)
