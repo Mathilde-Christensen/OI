084a31bbda45a5f2d648275a96b892a028b111ff
Mathilde-Christensen <mathildemc27@gmail.com>
2025-10-28 23:12:59 +0100

    Tilføje test knap

--- src/views/TestM.vue
+++ src/views/TestM.vue
@@ -4,6 +4,11 @@
 
 <template>
     <div>
-        <BookButton />
+        <BookButton id="test_evt" />
     </div>
+
+      <div style="padding: 2rem">
+    <!-- Du giver selv et midlertidigt ID -->
+    <BookButton id="test_evt" />
+  </div>
 </template>
