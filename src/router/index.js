e643c676f4d4bb6020674af21a7b1e394f23c094
k1rja <kirjabanke1999@gmail.com>
2025-10-28 09:14:29 +0100

    slettet filer

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import CalendarView from '../views/CalendarView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +9,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    },
-    {
-      path: '/calendar',
-      name: 'calendar',
-      component: CalendarView,
     },
   ],
 })
