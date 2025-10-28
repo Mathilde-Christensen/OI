d8c7fdcc9967b5172c8460fddf38d94fb5a796b5
Mathilde-Christensen <mathildemc27@gmail.com>
2025-10-28 11:12:37 +0100

    main.js kald til main.scss

---
+++ src/views/TestM.vue
@@ -0,0 +1,14 @@
+<script setup>
+ import BookBtm from '../components/BookBtm.vue'
+
+    const onClick = () => {
+    console.log('BookBtm klikket!')
+    }
+</script>
+
+<template>
+      <div style="padding:2rem">
+    <h1>TestM</h1>
+    <BookBtm @click="onClick" />
+  </div>
+</template>
