dd39d0110882b51e7979f4eaf257553f481b365c
Mathilde-Christensen <mathildemc27@gmail.com>
2025-10-28 12:53:04 +0100

    det virker sku

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    { path: '/testm',
+    { path: '/testpage',
       name: 'testm', 
       component: TestM,
     },
