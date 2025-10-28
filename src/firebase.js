cd0c860fa99d73dcf48df7a5a730037bea964975
k1rja <kirjabanke1999@gmail.com>
2025-10-28 12:44:20 +0100

    pinia slettet og mount slettet

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
-// (valgfrit til senere login hvis I vil): 
-// import { getAuth } from 'firebase/auth'
+
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
