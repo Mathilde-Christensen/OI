b54724a79ee0d6cdd5e50bda0ff46a42ab9a5b2c
Mathilde-Christensen <mathildemc27@gmail.com>
2025-11-14 08:47:41 +0100

    opsætning af styrke siden

--- src/views/MoveTraining.vue
+++ src/views/MoveTraining.vue
@@ -23,41 +23,47 @@ import Styrke from '@/assets/images/activities/styrke.webp'
   title="Styrketræning"
   text="Find de foreninger som tilbyder aktiviteter hvor der er fokus på styrketræning. "
   buttonText="Udforsk"
+  buttonLink="/foreningsstrength"
 />
 
 <SectionDarkBlue
-    :image="Gymnastik"
-    title="Gymnastik"
-    text="Find nogen af de mange foreninger som tilbyder gymnastik hold. Her finder du blandt andet tilbud om <span>rytmisk gymnastik, spring gymnastik, varmtvands gymnastik</span> og meget mere."
-    buttonText="Udforsk"
+  :image="Gymnastik"
+  title="Gymnastik"
+  text="Find nogen af de mange foreninger som tilbyder gymnastik hold. Her finder du blandt andet tilbud om <span>rytmisk gymnastik, spring gymnastik, varmtvands gymnastik</span> og meget mere."
+  buttonText="Udforsk"
+  buttonLink="/foreningsstrength"
 />
 
 <SectionLightBlue
   :image="Dans"
-    title="Dans"
-    text="Find de foreninger som tilbyder dansehold, her finder du blandt andet <span>cheerleding, dance-fitness og linedance</span>"
-    buttonText="Udforsk"
+  title="Dans"
+  text="Find de foreninger som tilbyder dansehold, her finder du blandt andet <span>cheerleding, dance-fitness og linedance</span>"
+  buttonText="Udforsk"
+  buttonLink="/foreningsstrength"
 />
 
 <SectionDarkBlue
-    :image="Atlatik"
-    title="Atletik"
-    text="Her finder du alle de forskellige foreninger som tilbyder Atletik aktiviteter "
-    buttonText="Udforsk"
+  :image="Atlatik"
+  title="Atletik"
+  text="Her finder du alle de forskellige foreninger som tilbyder Atletik aktiviteter "
+  buttonText="Udforsk"
+  buttonLink="/foreningsstrength"
 />
 
 <SectionLightBlue
   :image="Rulle"
-    title="Rulleskøjter"
-    text="Find de foreninger som tilbyder aktiviteter inde for rulleskøjtning som <span>rulleski, skøjtning og rulle skøjter</span>"
-    buttonText="Udforsk"
+  title="Rulleskøjter"
+  text="Find de foreninger som tilbyder aktiviteter inde for rulleskøjtning som <span>rulleski, skøjtning og rulle skøjter</span>"
+  buttonText="Udforsk"
+  buttonLink="/foreningsstrength"
 />
 
 <SectionDarkBlue
-    :image="Kampsport"
-    title="Kampsport"
-    text="Her finder du alle de forskellige foreninger som tilbyder forskellige kampsportshold. Blandt andet hold med <span>boksning, fætning, karate og mange flere</span>"
-    buttonText="Udforsk"
+  :image="Kampsport"
+  title="Kampsport"
+  text="Her finder du alle de forskellige foreninger som tilbyder forskellige kampsportshold. Blandt andet hold med <span>boksning, fætning, karate og mange flere</span>"
+  buttonText="Udforsk"
+  buttonLink="/foreningsstrength"
 />
 </template>
 
